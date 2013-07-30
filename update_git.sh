40fb068bc8c399e5bf38e3a4cfaf862324c91cb1
unknown <williamchew85@gmail.com>
2013-07-30 17:24:30 +0800

    Second Version

--- update_git.sh
+++ update_git.sh
@@ -1,7 +1,8 @@
-ssh -T git@github.com
+read -p "Enter commiting description: " comm_desc
+#ssh -T git@github.com
 git init
 git add *
-git commit -m "First Version"
+git commit -m "$comm_desc"
 git remote rm testapp
 git remote add testapp git@github.com:williamchew85/KendoUI_TestApp.git
 git pull testapp master
