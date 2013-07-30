f4b3e6a7278c2fdd8711e9403f9bc11288a8f9e2
unknown <williamchew85@gmail.com>
2013-07-30 15:45:32 +0800

    First Version

--- update_git.sh
+++ update_git.sh
@@ -3,6 +3,6 @@ git init
 git add *
 git commit -m "First Version"
 git remote rm testapp
-git remote add testapp https://github.com/williamchew85/KendoUI_TestApp
+git remote add testapp git@github.com:williamchew85/KendoUI_TestApp.git
 git pull testapp master
 git push testapp master
